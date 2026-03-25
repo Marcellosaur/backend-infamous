24a91b1099d441c3b3d540f74de14d197a057e72
Marcellosaur <marcellomorris7@gmail.com>
2026-03-24 19:53:53 -0500

    Initial commit of backend code

---
+++ server/src/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const db = require("./config/db");
+
+const app = express();
+
+const authRoutes = require("./routes/auth.routes");
+const bikesRoutes = require("./routes/bikes.routes");
+const favoritesRoutes = require("./routes/favorites.routes");
+const inquiriesRoutes = require("./routes/inquiries.routes");
+const adminRoutes = require("./routes/admin.routes");
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "*", // Replace * with your specific Vercel URL if you prefer hardcoding, or define FRONTEND_URL in Vercel
+  }),
+);
+app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "assets", "uploads")));
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+const { authenticate } = require("./middleware/auth.middleware");
+
+app.use("/api", authRoutes);
+app.use("/api", bikesRoutes);
+app.use("/api", favoritesRoutes);
+app.use("/api", inquiriesRoutes);
+app.use("/api/admin", adminRoutes);
+app.get("/api/protected", authenticate, (req, res) => {
+  res.json({ message: "You are authenticated!", user: req.user });
+});
+
+module.exports = app;
